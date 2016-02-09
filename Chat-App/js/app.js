366042c6cf059166fc5fe97005d5e6195a8d83a2
Ajay Poshak <poshakajay@gmail.com>
2016-02-09 10:47:15 +0530

    updated routing

--- Chat-App/js/app.js
+++ Chat-App/js/app.js
@@ -6,9 +6,9 @@ app.config(function($stateProvider,$urlRouterProvider){
             .state('login',{url:'/login',cache: false,templateUrl:'templates/login.html',controller:'loginCtrl'})
             .state('chat',{url:'/chat',cache: false,templateUrl:'templates/chat.html',controller:'chatCtrl'})
             .state('chatuser',{url:'/chatuser',cache: false,templateUrl:'templates/chatuser.html',controller:'chatuserCtrl'})
-            .state('group',{url:'/group',cache: false,templateUrl:'templates/group.html',controller:'groupCtrl'})
+            //.state('group',{url:'/group',cache: false,templateUrl:'templates/group.html',controller:'groupCtrl'})
             .state('addgroup',{url:'/addgroup',cache: false,templateUrl:'templates/addgroup.html',controller:'addgroupCtrl'})
             //.state('booking',{url:'/booking',templateUrl:'templates/bookingDetail.html',controller:'bookingDetailCtrl'})
             //.state('thanks',{url:'/thanks',templateUrl:'templates/thanks.html',controller:'thankCtrl'});
-    
+
     $urlRouterProvider.otherwise('/login');});
