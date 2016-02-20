bbbfd164153fa5e0d406bf18dca37ab1f45d7632
Ajay Poshak <poshakajay@gmail.com>
2016-02-20 21:11:22 +0530

    Removed Angular Folder Structure

--- Chat-App/js/Controllers/addcontactController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-angular.module("chatApp")
-  .controller('addcontactCtrl', addcontactCtrl);
-function addcontactCtrl($scope, $rootScope, $location, $window) {
-  $scope.BacktoEditGroup = function ()
-  {
-      $location.path('/editgroup');
-  };
-};
