8c445895b059cd8a2657ea059aaf4c665461b451
Ajay Poshak <poshakajay@gmail.com>
2016-02-27 23:01:13 +0530

    updated the redirections to group chat and member lists

--- ChatAppMeteor/client/scripts/Controllers/groupController.js
+++ ChatAppMeteor/client/scripts/Controllers/groupController.js
@@ -1,8 +1,37 @@
 "use strict";
 angular.module("chatApp")
   .controller('groupCtrl', groupCtrl);
-  function groupCtrl($scope, $rootScope, $location, $window) {
+  function groupCtrl($scope, $rootScope, $location, $state) {
     $rootScope.isConatct ='3'
+    var current = [];
+    var itr = 0;
+    var counter = 0;
+    var counter1 = 0;
+    $scope.defaultBlocks = [{}];
+    $scope.personalBlocks = [{}];
+    /*Getting data from userData in localStorage*/
+    if(!$rootScope.chatContacts){
+      current = $rootScope.chatContacts = JSON.parse(localStorage.getItem("userData"));
+      console.log("getting from local...");
+    }
+    else{
+        current = $rootScope.chatContacts;
+    }
+    console.log(current.data.blocks[0]);
+    for(itr in current.data.blocks){
+      console.log(current.data.blocks[itr].block_name);
+      if(current.data.blocks[itr].type === "default"){
+        $scope.defaultBlocks[counter] = current.data.blocks[itr];
+        //$scope.defaultBlocks[counter].block_name = current.data.blocks[itr].block_name;
+        counter++;
+      }
+      if(current.data.blocks[itr].type === "personal"){
+        console.log("personal::"+current.data.blocks[itr].block_id);
+        $scope.personalBlocks[counter1] = current.data.blocks[itr];
+        //$scope.personalBlocks[0].block_name = current.data.blocks[itr].block_name;
+        counter1++;
+      }
+    }
     $scope.BacktoChat = function ()
     {
         $location.path('/chat');
@@ -11,13 +40,24 @@ angular.module("chatApp")
     {
         $location.path('/addgroup');
     };
-    $scope.changePath = function ()
+    /*
+    *Redirect to group chat.
+    *@param {json} blockData - The data related to block which is selected.
+    */
+    $scope.changePath = function (blockData)
     {
-        $location.path('/chatuser');
+        var blockDataJson = JSON.stringify(blockData);
+        $state.go('chatuser', {groupDetailParam: blockDataJson });
     };
-    $scope.expandGroup = function ()
+    /*
+    * Redirects to show the group member details.
+    *@param {json} blockData - Data related to group which is selected.
+    */
+    $scope.expandGroup = function (blockData)
     {
-        $location.path('/groupwiseuser')
-    }
+      var blockDataJson = JSON.stringify(blockData);
+      console.log(blockDataJson);
+      $state.go('groupwiseuser', {blockDataParam: blockDataJson})
+    };
 
 };
