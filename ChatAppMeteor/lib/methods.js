592de6168997c8f11575527e6d460c7b62dda622
Ajay Poshak <poshakajay@gmail.com>
2016-03-05 22:19:14 +0530

    meteor method calls for message insertion in individual and group chats

--- ChatAppMeteor/lib/methods.js
+++ ChatAppMeteor/lib/methods.js
@@ -1,10 +1,20 @@
 Meteor.methods({
-  newMessage (message) {
-    message.timestamp = new Date();
-
-    let messageId = Messages.insert(message);
-    Chats.update(message.chatId, { $set: { lastMessage: message } });
-
-    return messageId;
+  'insertMessage': function(rec_id, sender_id, msg, image_id){
+    Chats.insert({
+      to: rec_id,
+      from: sender_id,
+      message: msg,
+      imageId: image_id,
+      createdAt: new Date()
+    });
+  },
+  'insertGroupMessage': function(block_id, user_id, username, msg){
+    Chats.insert({
+      to: block_id,
+      from: user_id,
+      fromName: username,
+      message: msg,
+      createdAt: new Date()
+    });
   }
 });
