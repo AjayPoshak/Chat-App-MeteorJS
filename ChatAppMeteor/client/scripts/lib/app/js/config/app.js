71d40874feee3a28a7cad902907fec141d7076ff
Ajay Poshak <poshakajay@gmail.com>
2016-03-19 14:31:39 +0530

    Routing of app

---
+++ ChatAppMeteor/client/scripts/lib/app/js/config/app.js
@@ -0,0 +1,17 @@
+angular.module('chatApp',['angular-meteor','ionic','ngResource','angularMoment', 'ngFileUpload','ngSanitize']);
+angular.module("chatApp")
+.config(function($stateProvider,$urlRouterProvider){
+    $stateProvider
+            .state('login',{url:'/login',cache: false,templateUrl:'client/templates/login.html',controller:'loginCtrl'})
+            .state('chat',{url:'/chat',cache: false,templateUrl:'client/templates/chat.html',controller:'chatCtrl'})
+            .state('chatuser',{url:'/chatuser',params: { userDetailParam: null, groupDetailParam: null},cache: false,templateUrl:'client/templates/chatuser.html',controller:'chatuserCtrl'})
+            .state('group',{url:'/group',cache: false,templateUrl:'client/templates/group.html',controller:'groupCtrl'})
+            .state('contact',{url:'/contact',cache: false,templateUrl:'client/templates/contact.html',controller:'contactCtrl'})
+            .state('addgroup',{url:'/addgroup',cache: false,templateUrl:'client/templates/addgroup.html',controller:'addgroupCtrl'})
+            .state('groupwiseuser',{url:'/groupwiseuser',params:{ blockDataParam: null},cache: false,templateUrl:'client/templates/groupwiseuser.html',controller:'groupwiseuserCtrl'})
+            .state('editgroup',{url:'/editgroup', params:{blockDataParam: null},cache: false,templateUrl:'client/templates/editgroup.html',controller:'editgroupCtrl'})
+            .state('addcontact',{url:'/addcontact', params:{blockIdParam: null},cache: false,templateUrl:'client/templates/addcontact.html',controller:'addcontactCtrl'})
+            //.state('booking',{url:'/booking',templateUrl:'templates/bookingDetail.html',controller:'bookingDetailCtrl'})
+            //.state('thanks',{url:'/thanks',templateUrl:'templates/thanks.html',controller:'thankCtrl'});
+
+    $urlRouterProvider.otherwise('/login');});
