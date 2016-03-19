be2746f3c6d415fde76f7a984d6594c9a88a7158
Ajay Poshak <poshakajay@gmail.com>
2016-03-19 14:30:02 +0530

    publishing blockeduser collection

--- ChatAppMeteor/server/Chats.js
+++ ChatAppMeteor/server/Chats.js
@@ -10,18 +10,29 @@ Meteor.publish("messages", function(){
   return Messages.find();
 });
 
-Meteor.publishComposite("instantMessages", {
-  find: function(){
-    return Chats.find();
-  },
-  children: [
-    {
-      find: function(chats){
-        return Images.find({_id: chats.imageId},
-        {
-          sort: {uploadedAt: -1}
-        });
-      }
-    }
-  ]
+Meteor.publish("blockedUsers", function(){
+  return BlockedUsers.find();
+})
+
+Meteor.publishComposite("instantMessages", function(user_id){
+  return {
+    find: function(){
+      return Chats.find({
+        $or: [
+          {"to": user_id},
+          {"from": user_id}
+        ]
+      });
+    },
+    children: [
+      {
+        find: function(chats){
+          return Images.find({_id: chats.imageId},
+            {
+              sort: {uploadedAt: -1}
+            });
+          }
+        }
+      ]
+    };
 });
