dc4a73325ea2083f3ed7b407504f88c61cba62bb
Ajay Poshak <poshakajay@gmail.com>
2016-04-15 14:19:55 +0530

    limitTo logic to improve the performance

--- ChatAppMeteor/client/scripts/Controllers/contactController.js
+++ ChatAppMeteor/client/scripts/Controllers/contactController.js
@@ -1,58 +1,55 @@
-
 "use strict";
 angular.module("chatApp")
   .controller('contactCtrl', contactCtrl);
 /*
 *@function: This is controller function for contacts page.
 */
-function contactCtrl($scope, $rootScope, $location, $window,$state) {
+function contactCtrl($scope, $rootScope,$timeout, $location, $window, $state) {
+  $scope.blockName = "All";
+  $scope.blockLimit = 2;
+  $scope.contactLimit = 10;
   var current = [];
   if(!$rootScope.chatContacts){
     current = $rootScope.chatContacts = JSON.parse(localStorage.getItem("userData"));
     console.log("getting from local...");
+    $timeout(function() {
+      $scope.blockLimit = 100;
+      $scope.contactLimit = 500;
+    }, 1000);
   }
   else{
       //console.log($rootScope.chatContacts);
       current = $rootScope.chatContacts;
+      $timeout(function() {
+      $scope.blockLimit = 100;
+      $scope.contactLimit = 500;
+    }, 1000);
   }
-  console.log(current.data.blocks[0]);
-  var blocks = [];
-  var users = [];
-  $scope.userInfo = [];
-  //console.log(current.data.blocks.length);
-  for(var i in current.data.blocks){
-    blocks[i] = current.data.blocks[i];
-    //console.log(current.blocks);
-  }
-  console.log(blocks[0]);
-  for(var j in blocks){
-    //console.log(blocks[j]);
-    //console.log(blocks[j].users);
-    //console.log(blocks[j].users.length);
-    for(var k=0; k<blocks[j].users.length; k++){
-      //console.log(blocks[j].users[k]);
-      if(blocks[j].type==="default"){
-          users[k] = blocks[j].users[k];
-          //console.log(k+" "+users[k]);
-      }
-    }
-  }
-  var itr = 0;
-  $scope.userInfo[0] = users[0];
-  for(var k in users){
-    //console.log("Users Length::"+users.length);
-    $scope.userInfo[itr] = users[k];
-    itr++;
-    /*for(var l=0; l<users.length; l++){
-      $scope.userInfo[itr] = users[k];
-      itr++;
-    }*/
-  }
-  /*Printing the userInfo*/
-  /*for(var itr in $scope.userInfo){
-    console.log(itr+" "+$scope.userInfo[itr]);
-  }*/
 
+  console.log(current);
+
+  $scope.blocks = [];
+  //console.log(current.data.blocks.length);
+  if(current !== undefined || current !== null){
+    for(var i in current.data.blocks){
+      $scope.blocks[i] = current.data.blocks[i];
+      console.log($scope.blocks[i].block_name);
+    };
+  }
+  /*To display the block names in drop down*/
+  $scope.blockNames = [];
+  /**
+   * Retrieve block names from block data
+   * these block names displayed in drop down
+   */
+  function getBlockNames(){
+    var itr = 0;
+    for(itr in $scope.blocks ){
+      console.log($scope.blocks[itr].block_name);
+      //$scope.blockNames[i] = $scope.blocks[itr].block_name;
+    }
+  };
+  $scope.isLoading = false;
   $rootScope.isConatct ='1'
   $scope.BacktoChat = function ()
   {
@@ -63,4 +60,23 @@ function contactCtrl($scope, $rootScope, $location, $window,$state) {
       var userParam=JSON.stringify(user);
       $state.go("chatuser",{userDetailParam:userParam})
   };
+  /**
+   * Calls the service to return all contact details
+   */
+  function getallContacts()
+    {
+      if($scope.individualChatFlag)
+      {
+        chatService.getallContact($rootScope.userInfo.user_id,$rootScope.userInfo.apartment_id)
+            .then(function(response){
+              $scope.Getallcontacts = response;
+              window.localStorage["userData"] = angular.toJson(response);
+            });
+      }
+    };
+    $scope.blockNameSelected = function(){
+      console.log("Block selected from drop down..."+$scope.blockName);
+    }
+    getallContacts();
+    //getBlockNames();
 };
